5e2f0a882338f906b418e3726e10af6ba862f35b
Bitheral <me@bitheral.net>
2025-05-26 16:39:17 +0100

    Silenced!

--- lxc/lemp.sh
+++ lxc/lemp.sh
@@ -40,15 +40,27 @@ description
 # ---- LEMP INSTALLATION START ----
 msg_info "Installing LEMP Stack"
 lxc-attach -n $CTID -- bash -c "
-  dpkg -l | grep php | tee packages.txt &&
-  apt install apt-transport-https &&
+  dpkg -l | grep php | tee packages.txt >/dev/null &&
+  apt-get install -qq -y apt-transport-https &&
   curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg &&
   sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list' &&
-  apt-get update &&
-  apt-get install -y nginx mariadb-server php8.3-fpm php8.3-mysql php8.3-cli php8.3-curl php8.3-mbstring php8.3-xml unzip &&
-  systemctl enable nginx &&
-  systemctl enable mariadb &&
-  systemctl enable php8.3-fpm
+  apt-get update -qq &&
+  DEBIAN_FRONTEND=noninteractive apt-get install -qq -y \
+    nginx mariadb-server php8.3-fpm php8.3-mysql php8.3-cli \
+    php8.3-curl php8.3-mbstring php8.3-xml unzip &&
+  systemctl enable nginx >/dev/null &&
+  systemctl enable mariadb >/dev/null &&
+  systemctl enable php8.3-fpm >/dev/null
+
+#  dpkg -l | grep php | tee packages.txt &&
+#  apt install apt-transport-https &&
+#  curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg &&
+#  sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list' &&
+#  apt-get update &&
+#  apt-get install -y nginx mariadb-server php8.3-fpm php8.3-mysql php8.3-cli php8.3-curl php8.3-mbstring php8.3-xml unzip &&
+#  systemctl enable nginx &&
+#  systemctl enable mariadb &&
+#  systemctl enable php8.3-fpm
 "
 msg_ok "Installed LEMP Stack"
 
