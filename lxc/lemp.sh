b1a34c1fb0a21d0e3fc92b041f0b1f185c55b6a0
Bitheral <me@bitheral.net>
2025-05-26 16:37:34 +0100

    Updated php to use 8.3

--- lxc/lemp.sh
+++ lxc/lemp.sh
@@ -40,8 +40,12 @@ description
 # ---- LEMP INSTALLATION START ----
 msg_info "Installing LEMP Stack"
 lxc-attach -n $CTID -- bash -c "
+  dpkg -l | grep php | tee packages.txt &&
+  apt install apt-transport-https &&
+  curl -sSLo /usr/share/keyrings/deb.sury.org-php.gpg https://packages.sury.org/php/apt.gpg &&
+  sh -c 'echo "deb [signed-by=/usr/share/keyrings/deb.sury.org-php.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list' &&
   apt-get update &&
-  apt-get install -y nginx mariadb-server php-fpm php-mysql php-cli php-curl php-mbstring php-xml unzip &&
+  apt-get install -y nginx mariadb-server php8.3-fpm php8.3-mysql php8.3-cli php8.3-curl php8.3-mbstring php8.3-xml unzip &&
   systemctl enable nginx &&
   systemctl enable mariadb &&
   systemctl enable php8.3-fpm
